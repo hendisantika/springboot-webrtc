172b93b92cc615429fc3f747f3e52277931f634e
Hendi Santika <hendisantika@gmail.com>
2020-12-28 17:37:27 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/springbootwebrtc/SpringbootWebrtcApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebrtc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebrtcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebrtcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootwebrtc/SpringbootWebrtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootwebrtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebrtcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
