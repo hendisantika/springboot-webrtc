bc1be45fe96fccffdf654561eff1ee1a0cadad15
Hendi Santika <hendisantika@gmail.com>
2020-12-28 17:43:04 +0700

    #8 - Add afterConnectionEstablished method in SignalingSocketHandler Class

--- src/main/java/com/hendisantika/springbootwebrtc/SignalingSocketHandler.java
+++ src/main/java/com/hendisantika/springbootwebrtc/SignalingSocketHandler.java
@@ -2,7 +2,12 @@ package com.hendisantika.springbootwebrtc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +25,31 @@ public class SignalingSocketHandler extends TextWebSocketHandler {
 
     private static final String TYPE_INIT = "init";
     private static final String TYPE_LOGOUT = "logout";
+
+    /**
+     * Cache of sessions by users.
+     */
+    private final Map<String, WebSocketSession> connectedUsers = new HashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        LOG.info("[" + session.getId() + "] Connection established " + session.getId());
+
+        // send the message to all other peers, that new men its being registered
+        final SignalMessage newMenOnBoard = new SignalMessage();
+        newMenOnBoard.setType(TYPE_INIT);
+        newMenOnBoard.setSender(session.getId());
+
+        connectedUsers.values().forEach(webSocketSession -> {
+            try {
+                webSocketSession.sendMessage(new TextMessage(Utils.getString(newMenOnBoard)));
+            } catch (Exception e) {
+                LOG.warn("Error while message sending.", e);
+            }
+        });
+
+        // put the session to the "cache".
+        connectedUsers.put(session.getId(), session);
+    }
+
 }
